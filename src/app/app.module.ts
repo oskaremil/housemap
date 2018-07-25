883821aa7be5799b2e47ef393431a907d1691b76
Oskar Emil Skeide <oskar.emil.skeide@avento.no>
2018-07-25 23:44:32 +0200

    angular cli template with bootstrap 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AreaComponent } from './area/area.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AreaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
